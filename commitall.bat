8235de4fd31c74009aeb90ba69bb6810ef1fff42
Patrick Jaja <patricks1987@web.de>
2014-12-25 12:56:58 +0100

    $1

--- commitall.bat
+++ commitall.bat
@@ -1,1 +1,2 @@
-git add -A && git commit -am "$1" && git push origin master
+echo "pso, neuen github Befehl anlegen für schnellen commit"
+git config --global alias.commitall "!func(){ git add -A && git commit -am '$1' && git push origin master; }; func"
